f8db991143d62f401ba90bf81dcaf867fd98e961
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2025-10-06 12:38:46 -0500

    Payment Flow Validation with Mocked Confirm API

--- features/submitting-payments/MakePayment.feature
+++ features/submitting-payments/MakePayment.feature
@@ -3,7 +3,7 @@ Feature: Make a payment
 
     As a customer, I should be able to make payments so I can enroll in the program.
 
-    #* AC1: When the user enters valid card information, checks the terms and conditions checkbox, 
+    #* AC1: When the user enters valid card information, checks the terms and conditions checkbox,
     #*      and clicks on the Pay button, then they should be redirected to the confirmation page.
 
     #* AC2: In the stepper, steps 1, 2, 3 should be green.
@@ -11,8 +11,33 @@ Feature: Make a payment
     #* AC4: The correct user email should be displayed.
     #* AC5: The correct company contact information should be displayed.
 
+    Background:
+        Given User is on the enrollment page
+        And User completed the start application step
+        And User completed the payment plan page
 
-    #TODO: Create scenarios that cover all the acceptance criteria
+    @sep23-1
+    Scenario: Verify User navigates to confirmation page when valid card information is entered and clicked Pay button
+        When User enters valid card information
+        And User checks on t&C check box
+        And Clicks on Pay button
+        Then Step three stepper circle should be green
+    @sep23-2
+    Scenario: Verify All Stepper Circles are Green
+        When User enters valid card information
+        And User checks on t&C check box
+        And Clicks on Pay button
+        Then Step one circle should be green
+        And Step two stepper circle should be green
+        And Step three stepper circle should be green
+    @sep23-3
+    Scenario: Verify program name user email and company contact are correctly displayed
+        When User enters valid card information
+        And User checks on t&C check box
+        And Clicks on Pay button
+        Then Program Name is displayed correctly
+        And User Email is displayed correctly
+        And Company contact information is displayed correctly
 
 
 
--- steps/submitting-payments/makePayment.js
+++ steps/submitting-payments/makePayment.js
@@ -1,4 +1,99 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
+import { expect } from "@playwright/test";
+import {
+    reviewPaymentPage,
+    startApplicationPage,
+    page,
+} from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+const jsonResponseBody = {
+    id: "pi_3SEygiJmxFuDfdzn1TPFf3or",
+    object: "payment_intent",
+    amount: 41200,
+    amount_details: {
+        tip: {},
+    },
+    automatic_payment_methods: {
+        allow_redirects: "always",
+        enabled: true,
+    },
+    canceled_at: null,
+    cancellation_reason: null,
+    capture_method: "automatic",
+    client_secret:
+        "pi_3SEygiJmxFuDfdzn1TPFf3or_secret_fr3JYr5IO6a2wUYdLDJxx6IsA",
+    confirmation_method: "automatic",
+    created: 1759696668,
+    currency: "usd",
+    description: "Test Automation with Selenium - one_time",
+    excluded_payment_method_types: null,
+    last_payment_error: null,
+    livemode: false,
+    next_action: null,
+    payment_method: "pm_1SEygiJmxFuDfdznuiMWbzZr",
+    payment_method_configuration_details: {
+        id: "pmc_1Olb9IJmxFuDfdznXRTQVXKM",
+        parent: null,
+    },
+    payment_method_types: ["card", "customer_balance"],
+    processing: null,
+    receipt_email: null,
+    setup_future_usage: null,
+    shipping: null,
+    source: null,
+    status: "succeeded",
+};
+When("User enters valid card information", async function () {
+    await reviewPaymentPage.enterCardNumber(process.env.CARD_NUMBER);
+    await reviewPaymentPage.enterExpiryDate(process.env.EXPIRATION_DATE);
+    await reviewPaymentPage.enterCVC(process.env.CVC);
+    await reviewPaymentPage.enterZipCode(process.env.ZIP_CODE);
+});
+// TODO: Currently being Mocked since Pay API is Failing in QA Environment
+When("Clicks on Pay button", async function () {
+    const targetUrl = "**/v1/payment_intents/*/confirm";
+    await page.route(targetUrl, async (route) => {
+        const request = route.request();
+        console.log(`\n--- Intercepted Request ---`);
+        console.log(`Method: ${request.method()}`);
+        console.log(`URL: ${request.url()}`);
+        await route.fulfill({
+            status: 200,
+            contentType: "application/json",
+            body: JSON.stringify(jsonResponseBody),
+        });
+        console.log(`✅ Request to ${route.request().url()} was mocked.`);
+    });
+    await reviewPaymentPage.clickPayButton();
+    await page.waitForResponse(targetUrl);
+});
+
+Then("Step three stepper circle should be green", async function () {
+    await expect(startApplicationPage.reviewStepCircle).toHaveCSS(
+        "background-color",
+        "rgb(172, 245, 138)"
+    );
+});
+
+Then("Program Name is displayed correctly", async function () {
+    const actualProgramName = productInfo.programName;
+    await expect(
+        page.locator("span").filter({ hasText: actualProgramName })
+    ).toBeVisible();
+});
+
+Then("User Email is displayed correctly", async function () {
+    const actualEmail = await page
+        .locator("div.payment-confirmation")
+        .locator("u")
+        .innerText();
+    expect(actualEmail).toEqual("jsahoo@apple.com.");
+});
+
+Then("Company contact information is displayed correctly", async function () {
+    const footerTextLocator = page.locator("p.footer-text").last();
+    await expect(footerTextLocator).toHaveText(
+        "Need help? Contact us at enrollment@cydeo.com"
+    );
+});
