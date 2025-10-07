4fd3f1a09f03b89f756b43eceba69b94272ed1df
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2025-10-07 09:06:53 -0500

    Step1 Next Button with and without optional field validation

--- features/getting-started/ClickNextButtonStep1.feature
+++ features/getting-started/ClickNextButtonStep1.feature
@@ -6,9 +6,21 @@ Feature: Click on the next button on step 1
     #* AC1: The next button should take customers to step 2 when given valid information.
     #*              a. Test by providing all fields
     #*              b. Test by providing only the required fields
-    
+
 
     #TODO: Create scenarios that cover all the acceptance criteria
 
     Background:
         Given User is on the enrollment page
+    @sep19-1
+    Scenario: Submit enrollment from with all valid Information
+        When User provides valid information for all required fields including optional information
+        And Clicks on Next button on Step one
+        Then Step one stepper circle should be green
+        And Step two stepper circle should be blue
+    @sep19-2
+    Scenario: Submit enrollment from with only required valid information
+        When User provides valid information for all required fields without optional information
+        And Clicks on Next button on Step one
+        Then Step one stepper circle should be green
+        And Step two stepper circle should be blue
--- steps/getting-started/clickNextButtonStep1.js
+++ steps/getting-started/clickNextButtonStep1.js
@@ -1,5 +1,36 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
 
+When(
+    "User provides valid information for all required fields including optional information",
+    async function () {
+        await startApplicationPage.enterFirstName("Jyoti");
+        await startApplicationPage.enterLastName("Sahoo");
+        await startApplicationPage.enterEmail("jsahoo@apple.com");
+        await startApplicationPage.enterPhoneNumber("4083323363");
+        await startApplicationPage.selectHowDidYouHearAboutUs("linkedin");
+    }
+);
+
+When("Clicks on Next button on Step one", async function () {
+    await startApplicationPage.clickNextButton();
+});
+
+Then("Step one stepper circle should be green", async function () {
+    await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+        "background-color",
+        "rgb(172, 245, 138)"
+    );
+});
+
+When(
+    "User provides valid information for all required fields without optional information",
+    async function () {
+        await startApplicationPage.enterFirstName("Jyoti");
+        await startApplicationPage.enterLastName("Sahoo");
+        await startApplicationPage.enterEmail("jsahoo@apple.com");
+        await startApplicationPage.enterPhoneNumber("4083323363");
+    }
+);
